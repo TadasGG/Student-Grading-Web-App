3ee4ef4258da050d0378170db6fe78fcafb91915
TadasGG <grigaitis.t@gmail.com>
2026-04-01 12:35:29 +0300

    Delete BackendServer directory

--- BackendServer/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import getUsers, addUser, editStudent
-
-urlpatterns = [
-    path('students', getUsers, name='getUser'),
-    path('students/create', addUser, name='addUser'),
-    path('students/<int:pk>', editStudent, name='editStudent')
-]
